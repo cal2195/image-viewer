d200f0895b9fce5b5d84071afc69a59ae90fa7ab
cal2195 <calmart101@gmail.com>
2019-05-17 13:27:45 +0100

    wip close

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -42,6 +42,15 @@ export class HomeComponent implements OnInit {
     private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
+    window.onbeforeunload = (e) => {
+      console.log('I do not want to be closed');
+
+      // Unlike usual browsers that a message box will be prompted to users, returning
+      // a non-void value will silently cancel the close.
+      // It is recommended to use the dialog API to let the user confirm closing the
+      // application.
+      e.returnValue = false; // equivalent to `return false` but not recommended
+    };
     this.updateQueue.drain = () => {
       this.treeview.tree.treeModel.update();
       this.cdr.detectChanges();
