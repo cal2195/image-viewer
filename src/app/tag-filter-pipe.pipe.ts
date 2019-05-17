246bbaa9a88f5f952654d39225b9a71612bd529e
cal2195 <calmart101@gmail.com>
2019-05-17 16:08:16 +0100

    Support basic boolean operations in search

--- src/app/tag-filter-pipe.pipe.ts
+++ src/app/tag-filter-pipe.pipe.ts
@@ -13,18 +13,32 @@ export class TagFilterPipePipe implements PipeTransform {
     const searchTags = tags.split(' ');
     return items.filter((item) => {
       if (item.tags) {
+        let atLeastOne = false;
         for (let i = 0; i < searchTags.length; i++) {
           const tag = searchTags[i];
-          if (tag === '') {
+          if (tag === '' || tag === '-') {
             continue;
           }
-          console.log(searchTags);
-          console.log(item.tags);
-          if (item.tags.indexOf(tag) === -1) {
-            return false;
+          if (tag.charAt(0) === '+') {
+            console.log('must include %s', tag);
+            atLeastOne = true;
+            if (item.tags.indexOf(tag.substring(1)) === -1) {
+              return false;
+            }
+          } else if (tag.charAt(0) === '-') {
+            console.log('must not include %s', tag);
+            atLeastOne = true;
+            if (item.tags.indexOf(tag.substring(1)) !== -1) {
+              return false;
+            }
+          } else {
+            console.log('can include %s', tag);
+            if (item.tags.indexOf(tag) !== -1) {
+              atLeastOne = true;
+            }
           }
         }
-        return true;
+        return atLeastOne;
       }
       return false;
     });
